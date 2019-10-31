bb90cd48b3c5fa02bbbe6b4a7333a5c63a6e8fc9
BIu Bill <zlx630416@gmail.com>
2019-10-31 11:04:50 -0700

    final version

--- assignment2/js/style.js
+++ assignment2/js/style.js
@@ -1,3 +1,4 @@
+
 function clickMenu(){
 	// var menu = document.getElementById('burger')
 	var burger = document.querySelector('.burger');
@@ -13,38 +14,16 @@ function clickMenu(){
 			nav.classList.toggle('nav-active');
 		});
 	}
-	navMenus.forEach((link,index) => {
-		if(link.style.animation){
-			link.style.animation = '';
-		}else{
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}`;
-		}
-		//Burger Animation
-		burger.classList.toggle('toggle');
-	})
+	// navMenus.forEach((link,index) => {
+	// 	if(link.style.animation){
+	// 		link.style.animation = '';
+	// 	}else{
+	// 		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}`;
+	// 	}
+	// 	//Burger Animation
+	// 	burger.classList.toggle('toggle');
+	// })
 }
 
 
 
-
-// const navSlide =()=> {
-// 	const burger = document.querySelector('.burger');
-// 	const nav = document.querySelector('.top-menu-nav');
-// 	const navMenus = document.querySelectorAll('.top-menu-nav li');
-// 	//Toggle Nav
-// 	burger.addEventListener('click', ()=>{
-// 		nav.classList.toggle('nav-active');
-// 	});
-// 	//Animate Links
-// 	navMenus.forEach((link,index) =>{
-// 		if(link.style.animation){
-// 			link.style.animation = '';
-// 		}else{
-// 			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}`;
-// 		}
-// 		//Burger Animation
-// 		burger.classList.toggle('toggle');
-// 	});
-// }
-
-// navSlide();
